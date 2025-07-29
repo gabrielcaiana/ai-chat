f246e493ea4305c74621b6e8888df9c185252781
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2025-07-29 06:44:25 -0300

    chore: add TypeScript configuration and logger middleware for server layer

---
+++ layers/base/server/middleware/logger.ts
@@ -0,0 +1,8 @@
+export default defineEventHandler(async (_e) => {
+  // const storage = useStorage("db");
+  // await storage.setItem(`telemetry:request:${Date.now()}`, {
+  //   url: getRequestURL(e),
+  //   method: e.method,
+  //   headers: getRequestHeaders(e),
+  // });
+});
